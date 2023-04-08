6f857433c82e9b153749ed48560d8996fa347e3e
Dionei Bianchati <dion.biancha@gmail.com>
2023-04-08 17:22:15 -0300

    feat: separating views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 
-export const home = (req: Request, res: Response) => {};
-export const dogs = (req: Request, res: Response) => {};
+export const home = (req: Request, res: Response) => {
+  res.render("pages/page");
+};
+export const dogs = (req: Request, res: Response) => {
+  res.send("dogs");
+};
 export const cats = (req: Request, res: Response) => {};
 export const fishes = (req: Request, res: Response) => {};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,10 +5,10 @@ import * as SearchController from "../controllers/searchController";
 const router = Router();
 
 router.get("/", PageController.home);
-router.get("/", PageController.dogs);
-router.get("/", PageController.cats);
-router.get("/", PageController.fishes);
+router.get("/dogs", PageController.dogs);
+router.get("/cats", PageController.cats);
+router.get("/fishes", PageController.fishes);
 
-router.get("/", SearchController.search);
+router.get("/search", SearchController.search);
 
 export default router;
